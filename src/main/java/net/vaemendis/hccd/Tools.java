53e05dd816e1594d67bfeba9cac1ea42df39d0d6
Tom <tom@ftml.net>
2017-12-09 23:05:42 +0100

    Make HCCD compatible with Java 9

--- src/main/java/net/vaemendis/hccd/Tools.java
+++ src/main/java/net/vaemendis/hccd/Tools.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class Tools {
 
     public static java.util.List<Image> getApplicationIcons() throws IOException {
-        Object o = new Object();
+        Object o = new Tools();
         java.util.List<Image> iconList = new ArrayList<>();
         iconList.add(ImageIO.read(o.getClass().getResource("/icon-256.png")));
         iconList.add(ImageIO.read(o.getClass().getResource("/icon-128.png")));
